fb02dc57049eba4fc3ef4d060ec6978df31f8b44
AlexBelozertsev <kashim983@gmail.com>
2021-03-06 18:05:51 +0200

    tiny improve

--- src/components/Feedback/FeedbackOptions.js
+++ src/components/Feedback/FeedbackOptions.js
@@ -6,7 +6,7 @@ const FeedbackOptions = ({ options, onLeaveFeedback }) => (
   <div className={style.Feedback__bar}>
     {Object.keys(options).map(key => (
       <button key={key} type="button" onClick={onLeaveFeedback}>
-        {key[0].toUpperCase() + key.slice(1).toLowerCase()}
+        {key[0].toUpperCase() + key.slice(1)}
       </button>
     ))}
   </div>
